06f8c7d89969bfb64da2f16d1dfd91578af4cb43
SravaniPaturu <sravani.paturu190103@gmail.com>
2023-11-17 10:23:39 +0530

    Program for ATMInterface

--- ATMInterface.java
+++ ATMInterface.java
@@ -1,18 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class ATMInterface
-{
-
-    public static void main(String args[])
+{  
+     public static void main(String args[])
     {
-        int balance=0,withdraw_amount,deposit_amount;
+
+        int balance=0,withdraw_amount,deposit_amount,transfer_amount;
+        String accN1="x1234",accN2="x3456",AccountNum,PinNum;
+        String pin="5678";
+
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your Account Number:");
+        AccountNum=sc.nextLine();
+        System.out.print("Enetr your pin number:");
+        PinNum=sc.nextLine();
+        ArrayList<String> l=new ArrayList<>();
+        l.add("Account Number:"+accN1);
+        l.add("Pin Number:"+pin);
+        System.out.println("Account Details:"+l);
         
+
+        if(AccountNum.equals(accN1) && PinNum.equals(pin))
+        {
         while(true)
         {
         System.out.println("ATM MACHINE\n");
 
-        System.out.println("Choose 1 for Withdraw\n"+"Choose 2 for Deposit\n"+"Choose 3 for Check Balance\n"+"Choose 4 for EXIT\n");
+        System.out.println("Choose 1 for Deposit\n"+"Choose 2 for Withdraw\n"+"Choose 3 for Check Balance\n"+"Choose 4 for Fund Transfer"+"Choose 5 for EXIT\n");
 
         System.out.print("Choose the operation:");
         
@@ -20,7 +34,15 @@ public class ATMInterface
         int choice=sc.nextInt();
         switch(choice)
         {
-        case 1: System.out.print("Enter money to be withdraw:");
+        case 1: System.out.print("Enter money to be deposited:");
+                deposit_amount=sc.nextInt();
+                balance=balance+deposit_amount;
+                System.out.println(deposit_amount+"has been successfully deposited into Account Number"+AccountNum);
+                System.out.println("Your updated balance is:"+balance);
+                break;
+        
+
+        case 2: System.out.print("Enter money to be withdraw:");
                 withdraw_amount=sc.nextInt();
 
                 if(balance<=withdraw_amount)
@@ -30,31 +52,40 @@ public class ATMInterface
                 else
                 {
                     balance=balance-withdraw_amount;
-                    System.out.println("Your has been successfully debited\n");
+                    System.out.println(withdraw_amount+"has been successfully debited from Account Number"+AccountNum);
+                    System.out.println("Your updated balance is:"+balance);
                 }
                 break;
-        
-
-        case 2: System.out.print("Enter money to be deposited:");
-                deposit_amount=sc.nextInt();
-                balance=balance+deposit_amount;
-                System.out.println("Your money has been successfully deposited");
+        case 3: System.out.println("Balance"+":"+balance);
                 break;
 
-        case 3:
-        
-             System.out.println("Balance"+":"+balance);
-             break;
+        case 4: System.out.print("Enter money to be transfered:");
+                transfer_amount=sc.nextInt();
+                
+                if(transfer_amount<balance)
+                {
+                    System.out.println("Insufficient balance");
+                }
+                else
+                {
+                    balance=balance-transfer_amount;
+                    System.out.println(transfer_amount+"has been transfered from Account Number"+AccountNum+"to Account number"+accN2);
+                    System.out.println("Your updated balance is:"+balance);
+                }
+                break;
 
-        case 4:
-        
-             
-             System.exit(0);
+        case 5: System.exit(0);
 
             }
         }
-        
     }
+
+    else
+    {
+        System.out.println("Your Entered wrong information");
+    }
+        
+}   
 
    
 }
