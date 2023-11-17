7828e6b84f5862426d87160007d31f6fb153eceb
SravaniPaturu <sravani.paturu190103@gmail.com>
2023-11-17 10:59:08 +0530

    Program for ATMInterface

--- ATMInterface.java
+++ ATMInterface.java
@@ -93,7 +93,7 @@ public class ATMInterface
                     }
                     else
                     {
-                         System.err.println("Invalid Account Number");
+                         System.out.println("Invalid Account Number");
                     }
                 }
                 break;
